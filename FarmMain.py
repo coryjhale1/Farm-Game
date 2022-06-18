9033f60b69aef819eb35c2fdd9873c75666fa031
Cory Hale <105729460+coryjhale1@users.noreply.github.com>
2022-06-18 12:28:05 -0400

    added farmer seed planting method and seed growing visual method

--- FarmMain.py
+++ FarmMain.py
@@ -4,20 +4,23 @@ import time
 
 class Farmer:
 
-    def __init__(self, input_name, input_money, planted_seeds = 0, owns_land = False):
+    def __init__(self, input_name, input_money, owns_land):
         self.name = input_name
         self.money = input_money
         self.seeds = [[watermelon, 0], [carrot, 0], [lettuce, 0]]
         self.crops = []
         self.land = 0
         self.landOwner = owns_land
-        self.planted = planted_seeds
-        self.ownsLand = owns_land
 
 # A descriptive function for the farmer class
     def __repr__(self):
         os.system("clear")
-        print("Farmer {name} has {acres} acres of land, {money} dollar, {seed_number} seeds, and {veg_number} veggies.".format(name=self.name, acres = self.land, money= self.money, seed_number = (self.seeds[0][1] + self.seeds[1][1] + self.seeds[2][1]),  veg_number = len(self.crops)))
+
+        if (self.seeds[0][1] + self.seeds[1][1] + self.seeds[2][1]) > 1 or (self.seeds[0][1] + self.seeds[1][1] + self.seeds[2][1]) == 0:
+            print("Farmer {name} has {acres} acres of land, {money} dollar, {seed_number} seeds, and {veg_number} veggies.".format(name=self.name, acres = self.land, money= self.money, seed_number = (self.seeds[0][1] + self.seeds[1][1] + self.seeds[2][1]),  veg_number = len(self.crops)))
+
+        else:
+            print("Farmer {name} has {acres} acres of land, {money} dollar, {seed_number} seed, and {veg_number} veggies.".format(name=self.name, acres = self.land, money= self.money, seed_number = (self.seeds[0][1] + self.seeds[1][1] + self.seeds[2][1]),  veg_number = len(self.crops)))
 
 
         any_key = input("Press enter to continue ")
@@ -67,7 +70,7 @@ class Farmer:
                     if land_cost <= self.money:
                         self.land += first_buy
                         self.money -= land_cost
-                        self.landOwner = True
+                        self.landOwner += True
                         print("You now have {acres} acres and ${money}!".format(acres = self.land, money = self.money))
                         time.sleep(2)
                         return beginning()
@@ -144,7 +147,7 @@ class Farmer:
                         self.money -= (seeds.cost * how_many)
                         self.seeds[counted_seeds][1] += how_many
                         seeds.stock -= how_many
-                        print("You have bought seeds.")
+                        print("You have bought {number} seeds.".format(number = how_many))
                         # print("You now have {number} {type} seeds and ${money}".format(number = self.seeds))
                         time.sleep(2)
                     elif (price_confirmation.upper() == "NO" or "N"):
@@ -196,37 +199,91 @@ class Farmer:
 
 # Defines the planting function
     def plant_seeds(self, seeds):
-        if (len(self.seeds == 0)):
+
+        os.system("clear")
+        counted_seeds = self.seeds.count(seeds)
+        print(counted_seeds)
+        seed_count = self.seeds[counted_seeds][1]
+
+        # Error Message
+        if ((self.seeds[0][1] + self.seeds[1][1] + self.seeds[2][1]) <= 0):
             print("You must first buy seeds.")
-            return 
+            return beginning()
+
+        # Main Track
         else:
-            if (self.seeds == 0):
-                print("You do not have any of that seed")
-                return 
+            if self.landOwner == True:
+
+                how_many_input = input("How many would you like to plant? ")
+                if (how_many_input.isnumeric()):
+
+                    how_many = int(how_many_input)
+                    if how_many > seeds.acre * self.land:
+                        print("That many seeds wont fit on your land. Check carrying capacity and try again.")
+                        time.sleep(2)
+                        return beginning()
+
+                    elif how_many <= 0:
+                        print("You have not planted anything")
+                        time.sleep(2)
+                        return beginning()
+                    
+                    else:
+                        seed_count -= how_many
+                        seeds.grow()
+                        time.sleep(2)
+                        return beginning()
+                    
+
+                # Error Message
+                else:
+                    print("Invalid response")
+                    time.sleep(2)
+                    return self.plant_seeds()  
+
+            # Error Message
             else:
-                self.seeds -=  how_many
-                print("Seeds planted!")
-                return 
-            return seed_option
+                print("You must own land to plant seeds")
+                time.sleep(2)
+                return beginning()
 
 
 class Seeds:
-
-    def __init__(self, input_name, input_cost, input_growth, stock_total, grow_time,per_acre, in_stock = True):
+    def __init__(self, input_name, input_cost, stock_total, grow_time, per_acre, in_stock = True, growing = True):
         self.name = input_name
         self.cost = input_cost
-        self.growth = input_growth
+        self.time = grow_time
         self.stock = stock_total
+        self.acre = per_acre
+
+    def grow (self):
+        i = 0
+        while i < self.time:
+            print(str(i))
+            time.sleep(1)
+            i += 1
+        print("CROPS")
+        
+
+        
     
 
 
 
-# main gameplay setup
-watermelon = Seeds("watermelon", 3, 20, 100, 20, 15)
-carrot = Seeds("carrot", 1, 5, 100, 10, 25)
-lettuce = Seeds("lettuce", 2, 3, 100, 15, 20)
+class Store:
+    def restock_store(seeds):
+        pass
 
+
+
+# Seed Classes
+watermelon = Seeds("watermelon", 3, 100, 20, 15)
+carrot = Seeds("carrot", 1, 100, 10, 25)
+lettuce = Seeds("lettuce", 2, 100, 15, 20)
+
+# Start Screen setup
 def beginning():
+
     os.system("clear")
     print(
         """
@@ -242,47 +299,64 @@ def beginning():
     )
     start_screen = int(input("\nWhat would like to do? \n \nEnter:\n1 for TRADE LAND\n2 for BUY SEEDS\n3 for PLANT CROP\n4 for HARVEST CROP\n5 for SELL CROP\n6 for CHECK FARMER STATUS\n7 for QUIT GAME\n\n"))
 
+    # Buy/Sell land
     if start_screen == 1:
-        # print("1")
         farmer.trade_land()
     
+    # Buy Seeds
     if start_screen == 2:
         os.system("clear")
         print("""
  ____________________________________________________ 
 |  ________________________________________________  |
-| |                  SEED PRICES                   | |
-                    WATERMELON: $3
-                      CARROT: $1
-                      LETTUCE: $2 
+| | _________________SEED PRICES___________________| |
+                    
+           SEED    |   PRICE   |     STOCK
+        WATERMELON |    $3     |      {watermelon}
+          CARROT   |    $1     |      {carrot}
+         LETTUCE   |    $2     |      {lettuce}
 | |________________________________________________| |
 |____________________________________________________|
-        """)
+        """.format(watermelon = watermelon.stock, carrot = carrot.stock, lettuce = lettuce.stock))
+
         seed_choice = input("What kind of seeds would you like?\n\nEnter:\n{seed_1}\n{seed_2}\n{seed_3}\n\n".format(seed_1 = watermelon.name, seed_2 = carrot.name, seed_3 = lettuce.name))
+
         if seed_choice.lower().strip() == watermelon.name or carrot.name or lettuce.name:
             farmer.buy_seed(globals()[seed_choice.lower().strip()])
         else:
             print("Invalid response")
+            time.sleep(2)
             return beginning()
 
+    # Plant crops
     if start_screen == 3:
-        print("3")
-        time.sleep(2)
-        return beginning()
-     
+        seed_choice = input("What kind of seeds would you like to plant?\n\nEnter:\n{seed_1}\n{seed_2}\n{seed_3}\n\n".format(seed_1 = watermelon.name, seed_2 = carrot.name, seed_3 = lettuce.name))
+
+        if seed_choice.lower().strip() == watermelon.name or carrot.name or lettuce.name:
+            farmer.plant_seeds(globals()[seed_choice.lower().strip()])
+
+        else:
+            print("Invalid response")
+            time.sleep(2)
+            return beginning()
+
+    # Harvest crops 
     if start_screen == 4:
         print("4")
         time.sleep(2)
         return beginning()
 
+    # Sell Crops
     if start_screen == 5:
         print(farmer)
         return beginning()
-    
+
+    # Farmer status
     if start_screen == 6:
         print(farmer)
         return beginning()
 
+    # Quit game
     if start_screen == 7:
         os.system("clear")
         quit()
@@ -300,6 +374,6 @@ os.system("clear")
 
 farmer_input = input("Howdy farmer! What is your name? ")
 farmer_name = farmer_input.title().strip()
-farmer = Farmer(farmer_name, 100)
+farmer = Farmer(farmer_name, 100, False)
 
 beginning()
